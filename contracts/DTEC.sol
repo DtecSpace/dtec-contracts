42cd792e10b7c2be94f37ab9feafc9bedbd736ce
truthdeal <barisaryacantepe@gmail.com>
2023-11-17 09:07:48 +0100

    DTE-01 fix

--- contracts/DTEC.sol
+++ contracts/DTEC.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.17;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DTEC is ERC20 {
-    uint256 public MAX_SUPPLY = 900000000 * 10 ** 18;
+    uint256 public constant MAX_SUPPLY = 900000000 * 10 ** 18;
 
     constructor() ERC20("DTEC", "DTEC") {
         _mint(msg.sender, MAX_SUPPLY);
