0177c0527dd1057fdbdc1331cb19c3e49a05fa76
truthdeal <barisaryacantepe@gmail.com>
2023-11-20 15:03:18 +0100

    unused variable remove

--- contracts/vesting/Vesting.sol
+++ contracts/vesting/Vesting.sol
@@ -8,7 +8,6 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 contract Vesting is Ownable, ReentrancyGuard {
     mapping(address => LockInfo) public userToLockInfo;
     address public immutable dtecTokenAddress;
-    address public tokenLocker;
     uint256 public period = 30 days;
     uint256 public releaseRate;
     uint256 public lockStartTime;
