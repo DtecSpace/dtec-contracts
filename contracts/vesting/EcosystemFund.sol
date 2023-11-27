eeffb3805d1a773bf16faa3a1b86ee307eb70685
truthdeal <barisaryacantepe@gmail.com>
2023-11-27 15:09:28 +0100

    Airdrop and Ecosystem Fund seperated

--- contracts/vesting/EcosystemFund.sol
+++ contracts/vesting/EcosystemFund.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.17;
 
 import {Vesting} from "./Vesting.sol";
 
-contract Airdrop is Vesting {
+contract Ecosystem is Vesting {
     constructor(address _dtecAddress) Vesting(_dtecAddress) {
         setReleaseInfo(1751328001, 278);
     }
