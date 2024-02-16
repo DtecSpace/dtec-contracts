9602ae27695280a4ae0775ed8152bac60ab21718
truthdeal <barisaryacantepe@gmail.com>
2024-02-16 16:34:06 +0100

    presale changes

--- contracts/locks/PreSaleLock.sol
+++ contracts/locks/PreSaleLock.sol
@@ -50,6 +50,6 @@ contract PreSaleLock is LinearLock {
     constructor(address _dtecAddress) LinearLock(_dtecAddress) {
         // TGE is 01.04.2024, first lock release for Pre Sale is 01.10.2024
         // To get tokens on time, release lockTimestamp is 1725148801, 30 days before 01.10.2024
-        setReleaseInfo(1725148801, 1112);
+        setReleaseInfo(1725148801, 834);
     }
 }
