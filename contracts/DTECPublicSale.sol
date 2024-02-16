93050cf0b22d55229d0b6a0bb11afd80caa451f4
truthdeal <barisaryacantepe@gmail.com>
2024-02-16 16:39:42 +0100

    public sale changes

--- contracts/DTECPublicSale.sol
+++ contracts/DTECPublicSale.sol
@@ -46,10 +46,10 @@ import {DTECTokenSale} from './DTECTokenSale.sol';
 /// @notice This contract allows public users to buy or allocate tokens with restrictions on the minimum and maximum amounts.
 contract DTECPublicSale is DTECTokenSale {
     /// @notice Minimum amount of tokens a user can buy or allocate in one transaction
-    uint256 public constant MIN_TOKENS_TO_BUY = 820;
+    uint256 public constant MIN_TOKENS_TO_BUY = 980;
 
     /// @notice Maximum amount of tokens a user can buy or allocate in total
-    uint256 public constant MAX_TOKENS_TO_BUY = 83333;
+    uint256 public constant MAX_TOKENS_TO_BUY = 100000;
 
     /// @dev Mapping to track the amount of tokens bought by each address
     mapping(address => uint256) addressToBoughtAmt;
@@ -62,8 +62,8 @@ contract DTECPublicSale is DTECTokenSale {
     /// @param _dtecAddress Address of the DTEC token
     /// @param _lockerAddress Address of the locker contract
     constructor(address _receiver, address _dtecAddress, address _lockerAddress) DTECTokenSale(_receiver, _dtecAddress, _lockerAddress) {
-        setImmediateReleaseRate(1000); // Corresponding %10 at TGE
-        setSalePrice(120000); // Corresponding 0.12 USD, USDC and USDT have 6 decimals 
+        setImmediateReleaseRate(1500); // Corresponding %10 at TGE
+        setSalePrice(100000); // Corresponding 0.10 USD, USDC and USDT have 6 decimals 
     }
 
     /// @notice Allows a user to buy tokens with restrictions on the purchase amount
--- contracts/locks/PublicSaleLock.sol
+++ contracts/locks/PublicSaleLock.sol
@@ -50,6 +50,6 @@ contract PublicSaleLock is LinearLock {
     constructor(address _dtecAddress) LinearLock(_dtecAddress) {
         // TGE is 01.04.2024, first lock release for Public Sale is 01.06.2024
         // To get tokens on time, release lockTimestamp is 1714608001, 30 days before 01.06.2024
-        setReleaseInfo(1714608001, 2500);
+        setReleaseInfo(1714608001, 1250);
     }
 }
