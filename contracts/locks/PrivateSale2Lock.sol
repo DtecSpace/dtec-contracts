654a8ce952f7a6f295f3936a9380a01571f8e5c1
truthdeal <barisaryacantepe@gmail.com>
2024-03-22 13:54:25 +0300

    name change

--- contracts/locks/PrivateSale2Lock.sol
+++ contracts/locks/PrivateSale2Lock.sol
@@ -41,11 +41,11 @@ pragma solidity ^0.8.17;
 
 import {LinearLock} from "./LinearLock.sol";
 
-/// @title PreSaleLock
+/// @title PrivateSale2Lock
 /// @dev Extends LinearLock with specific settings for a presale lock.
 /// @notice Locks tokens for the presale phase and releases them linearly starting from a specific date.
-contract PreSaleLock is LinearLock {
-    /// @notice Constructor for PreSaleLock, initializing the lock with specific release timing and rate
+contract PrivateSale2Lock is LinearLock {
+    /// @notice Constructor for PrivateSale2Lock, initializing the lock with specific release timing and rate
     /// @param _dtecAddress Address of the DTEC token to be locked
     constructor(address _dtecAddress) LinearLock(_dtecAddress) {
         // TGE is 29.04.2024, first lock release for Private Sale 2 is 29.09.2024
