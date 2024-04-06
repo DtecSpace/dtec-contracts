f0f4462b7e3852bc97d158972255ace3131edba3
truthdeal <barisaryacantepe@gmail.com>
2024-04-06 02:57:11 +0200

    community changes

---
+++ contracts/locks/CommunitySaleLock.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+/* 
+
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*                                               
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@(                                     
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@.                                
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@,                             
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                           
+                       #@@@@@@@@@@@/     .,&@@@@@@@@@@@@@@@@@@@                         
+                       #@@@@@@@@@@@/            /@@@@@@@@@@@@@@@/                       
+                       #@@@@@@@@@@@/               #@@@@@@@@@@@@@&                      
+                                                     #@@@@@@@@@@@@&                     
+                                                       @@@@@@@@@@@@(                    
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*        @@@@@@@@@@@@                    
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*        @@@@@@@@@@@@                    
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*        #@@@@@@@@@@@                    
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*        @@@@@@@@@@@@                    
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*        @@@@@@@@@@@@                    
+                                                       @@@@@@@@@@@@/                    
+                                                     %@@@@@@@@@@@@&                     
+                       #@@@@@@@@@@@/               %@@@@@@@@@@@@@&                      
+                       #@@@@@@@@@@@/            %@@@@@@@@@@@@@@@,                       
+                       #@@@@@@@@@@@/     ,(@@@@@@@@@@@@@@@@@@@@                         
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&                           
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@.                             
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                                 
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*        
+            
+
+
+                ██████  ████████ ███████  ██████ 
+                ██   ██    ██    ██      ██      
+                ██   ██    ██    █████   ██      
+                ██   ██    ██    ██      ██      
+                ██████     ██    ███████  ██████ 
+                                
+                                 
+@author:   Baris Arya CANTEPE  (@bcantepe)
+*/
+pragma solidity ^0.8.17;
+
+import {LinearLock} from "./LinearLock.sol";
+
+/// @title CommunitySaleLock
+/// @dev Extends LinearLock with specific settings for a presale lock.
+/// @notice Locks tokens for the presale phase and releases them linearly starting from a specific date.
+contract CommunitySaleLock is LinearLock {
+    /// @notice Constructor for CommunitySaleLock, initializing the lock with specific release timing and rate
+    /// @param _dtecAddress Address of the DTEC token to be locked
+    constructor(address _dtecAddress) LinearLock(_dtecAddress) {
+        // TGE is 07.05.2024, first lock release for PreSale is 07.10.2024
+        // To get tokens on time, release lockTimestamp is 1725667200, 30 days before 07.10.2024
+        setReleaseInfo(1725667200, 834);
+    }
+}
