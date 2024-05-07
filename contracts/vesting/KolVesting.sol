49fb13fe321dc643d8c5ea7538aede0c2d701cfb
truthdeal <barisaryacantepe@gmail.com>
2024-05-07 12:27:38 +0200

    tge vesting contracts

---
+++ contracts/vesting/KolVesting.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+/* 
+
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*                                               
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@(                                     
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@.                                
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@,                             
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                           
+                       #@@@@@@@@@@@/     .,&@@@@@@@@@@@@@@@@@@@                         
+                       #@@@@@@@@@@@/            /@@@@@@@@@@@@@@@/                       
+                       #@@@@@@@@@@@/               #@@@@@@@@@@@@@&                      
+                                                     #@@@@@@@@@@@@&                     
+                                                       @@@@@@@@@@@@(                    
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*        @@@@@@@@@@@@                    
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*        @@@@@@@@@@@@                    
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*        #@@@@@@@@@@@                    
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*        @@@@@@@@@@@@                    
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*        @@@@@@@@@@@@                    
+                                                       @@@@@@@@@@@@/                    
+                                                     %@@@@@@@@@@@@&                     
+                       #@@@@@@@@@@@/               %@@@@@@@@@@@@@&                      
+                       #@@@@@@@@@@@/            %@@@@@@@@@@@@@@@,                       
+                       #@@@@@@@@@@@/     ,(@@@@@@@@@@@@@@@@@@@@                         
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&                           
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@.                             
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                                 
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*        
+            
+
+
+                ██████  ████████ ███████  ██████ 
+                ██   ██    ██    ██      ██      
+                ██   ██    ██    █████   ██      
+                ██   ██    ██    ██      ██      
+                ██████     ██    ███████  ██████ 
+                                
+                                 
+@author:   Baris Arya CANTEPE  (@bcantepe)
+*/
+pragma solidity ^0.8.17;
+
+import {TgeVestingBase} from "./TgeVesting.sol";
+
+/// @title KolRound
+/// @dev Extends the TgeVestingBase contract with specific settings for KolRound vesting.
+/// @notice Manages the vesting of tokens specifically allocated to the KolRound, with a distinct vesting schedule.
+contract KolRound is TgeVestingBase {
+    /// @notice Constructor for KolRound TgeVesting, initializing the vesting with specific timing and rate for the KolRound's tokens
+    /// @param _dtecAddress Address of the DTEC token to be vested for the KolRound
+    constructor(address _dtecAddress, uint256 _lockStartTime, uint256 _tgeReleaseRate, 
+    uint256 _releaseRate, uint256 _vestingAmount) 
+    
+    TgeVestingBase(
+        _dtecAddress,
+        _lockStartTime,
+        _tgeReleaseRate,
+        _releaseRate,
+        _vestingAmount
+    ) {}
+}
