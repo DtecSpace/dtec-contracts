bcad5d79f7d6e8a47eb6faa17e3d494d3d5fdb02
truthdeal <barisaryacantepe@gmail.com>
2024-07-02 16:37:58 +0200

    strategic vesting added

---
+++ contracts/vesting/StrategicVesting.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+/* 
+
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*                                               
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@(                                     
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@.                                
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@,                             
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                           
+                       #@@@@@@@@@@@/     .,&@@@@@@@@@@@@@@@@@@@                         
+                       #@@@@@@@@@@@/            /@@@@@@@@@@@@@@@/                       
+                       #@@@@@@@@@@@/               #@@@@@@@@@@@@@&                      
+                                                     #@@@@@@@@@@@@&                     
+                                                       @@@@@@@@@@@@(                    
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*        @@@@@@@@@@@@                    
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*        @@@@@@@@@@@@                    
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*        #@@@@@@@@@@@                    
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*        @@@@@@@@@@@@                    
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*        @@@@@@@@@@@@                    
+                                                       @@@@@@@@@@@@/                    
+                                                     %@@@@@@@@@@@@&                     
+                       #@@@@@@@@@@@/               %@@@@@@@@@@@@@&                      
+                       #@@@@@@@@@@@/            %@@@@@@@@@@@@@@@,                       
+                       #@@@@@@@@@@@/     ,(@@@@@@@@@@@@@@@@@@@@                         
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&                           
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@.                             
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                                 
+        (@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*        
+            
+
+
+                ██████  ████████ ███████  ██████ 
+                ██   ██    ██    ██      ██      
+                ██   ██    ██    █████   ██      
+                ██   ██    ██    ██      ██      
+                ██████     ██    ███████  ██████ 
+                                
+                                 
+@author:   Baris Arya CANTEPE  (@bcantepe)
+*/
+pragma solidity ^0.8.17;
+
+import {TgeVestingBase} from "./TgeVesting.sol";
+
+/// @title StrategicSale
+/// @dev Extends the TgeVestingBase contract with specific settings for StrategicSale vesting.
+/// @notice Manages the vesting of tokens specifically allocated to the StrategicSale, with a distinct vesting schedule.
+contract StrategicSale is TgeVestingBase {
+    /// @notice Constructor for StrategicSale TgeVesting, initializing the vesting with specific timing and rate for the StrategicSale's tokens
+    /// @param _dtecAddress Address of the DTEC token to be vested for the StrategicSale
+    constructor(address _dtecAddress, uint256 _lockStartTime, uint256 _tgeReleaseRate, 
+    uint256 _releaseRate, uint256 _vestingAmount) 
+    
+    TgeVestingBase(
+        _dtecAddress,
+        _lockStartTime,
+        _tgeReleaseRate,
+        _releaseRate,
+        _vestingAmount
+    ) {}
+}
