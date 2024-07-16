883cd5d501ccb13db2b8925006811d464aef2b5d
truthdeal <barisaryacantepe@gmail.com>
2024-07-16 12:18:31 +0200

    vesting name change

--- contracts/vesting/PrivateSaleVesting.sol
+++ contracts/vesting/PrivateSaleVesting.sol
@@ -41,19 +41,19 @@ pragma solidity ^0.8.17;
 
 import {TgeVestingBase} from "./TgeVesting.sol";
 
-/// @title PrivateSale
-/// @dev Extends the TgeVestingBase contract with specific settings for PrivateSale vesting.
-/// @notice Manages the vesting of tokens specifically allocated to the PrivateSale, with a distinct vesting schedule.
-contract PrivateSale is TgeVestingBase {
-    /// @notice Constructor for PrivateSale TgeVesting, initializing the vesting with specific timing and rate for the PrivateSale's tokens
-    /// @param _dtecAddress Address of the DTEC token to be vested for the PrivateSale
+/// @title PrivateSaleVesting
+/// @dev Extends the TgeVestingBase contract with specific settings for PrivateSaleVesting vesting.
+/// @notice Manages the vesting of tokens specifically allocated to the PrivateSaleVesting, with a distinct vesting schedule.
+contract PrivateSaleVesting is TgeVestingBase {
+    /// @notice Constructor for PrivateSaleVesting TgeVesting, initializing the vesting with specific timing and rate for the PrivateSaleVesting's tokens
+    /// @param _dtecAddress Address of the DTEC token to be vested for the PrivateSaleVesting
     constructor(address _dtecAddress, uint256 _vestingAmount) 
     
     TgeVestingBase(
         _dtecAddress,
         1726272000, // 14.10.2024 first token release
-        400, // %4 TGE for PrivateSale
-        960, // // 10 Vesting months for PrivateSale
+        400, // %4 TGE for PrivateSaleVesting
+        960, // // 10 Vesting months for PrivateSaleVesting
         _vestingAmount
     ) {}
 }
--- contracts/vesting/PublicSaleVesting.sol
+++ contracts/vesting/PublicSaleVesting.sol
@@ -41,19 +41,19 @@ pragma solidity ^0.8.17;
 
 import {TgeVestingBase} from "./TgeVesting.sol";
 
-/// @title PublicSale
-/// @dev Extends the TgeVestingBase contract with specific settings for PublicSale vesting.
-/// @notice Manages the vesting of tokens specifically allocated to the PublicSale, with a distinct vesting schedule.
-contract PublicSale is TgeVestingBase {
-    /// @notice Constructor for PublicSale TgeVesting, initializing the vesting with specific timing and rate for the PublicSale's tokens
-    /// @param _dtecAddress Address of the DTEC token to be vested for the PublicSale
+/// @title PublicSaleVesting
+/// @dev Extends the TgeVestingBase contract with specific settings for PublicSaleVesting vesting.
+/// @notice Manages the vesting of tokens specifically allocated to the PublicSaleVesting, with a distinct vesting schedule.
+contract PublicSaleVesting is TgeVestingBase {
+    /// @notice Constructor for PublicSaleVesting TgeVesting, initializing the vesting with specific timing and rate for the PublicSaleVesting's tokens
+    /// @param _dtecAddress Address of the DTEC token to be vested for the PublicSaleVesting
     constructor(address _dtecAddress, uint256 _vestingAmount) 
     
     TgeVestingBase(
         _dtecAddress,
         1721001600, // 14.08.2024 first token release
-        2000, // %20 TGE for PublicSale
-        1000, // // 8 Vesting months for PublicSale
+        2000, // %20 TGE for PublicSaleVesting
+        1000, // // 8 Vesting months for PublicSaleVesting
         _vestingAmount
     ) {}
 }
