f1864519a49c6a6005dd355c3a4d415c95d9900b
truthdeal <barisaryacantepe@gmail.com>
2024-10-04 14:54:25 +0300

    staking contacts

--- contracts/staking/Staking60Days.sol
+++ contracts/staking/Staking60Days.sol
@@ -3,14 +3,14 @@ pragma solidity ^0.8.17;
 
 import "./StakingBase.sol";
 
-contract DTEC30DaysStaking is StakingBase {
+contract DTEC60DaysStaking is StakingBase {
 
     constructor() StakingBase (
         IERC20(0xd87aF7B418d64FF2cdE48d890285bA64fc6E115F), // DTEC staking token
         IERC20(0xd87aF7B418d64FF2cdE48d890285bA64fc6E115F), // DTEC reward token 
-        50000, // 50% annual yield
-        30 days, // 30 days in seconds
-        1_000_000 * 10**18, // 1 million tokens
+        80000, // 80% annual yield
+        60 days, // 60 days in seconds
+        2_000_000 * 10**18, // 2 million tokens
         14 days // 14 days in seconds
     ) {
     }
